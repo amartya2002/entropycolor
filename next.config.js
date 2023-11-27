87a6cc6047825ddba74e8bbf0ab22a8cb7f71176
Amartya Sinha <amartua2002@gmail.com>
2023-11-27 18:11:08 +0530

    chore image optimization

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 const isProduction = process.env.NODE_ENV === "production";
 const assetPrefix = isProduction ? "/entropyui" : ""; 
 const nextConfig = {
-    images: {
-        unoptimized: true,
-      },
+    
       
       trailingSlash: true,
       assetPrefix,
