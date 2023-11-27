fb4d3611da1a417e3877127e56e8320c378983a5
Amartya Sinha <amartua2002@gmail.com>
2023-11-27 19:01:42 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 * @type {import('next').NextConfig}
 */
 const nextConfig = {
- 
+    images: {
+      loader: 'akamai',
+      path: '',
+    },
+    assetPrefix: './',
+  };
   
-};
-
-export default nextConfig;
+  export default nextConfig;
