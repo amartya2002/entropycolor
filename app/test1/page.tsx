4e03256eb0d61b8c2fc200e83db7cd2fa9d4b88e
Amartya Sinha <amartua2002@gmail.com>
2023-11-30 01:02:54 +0530

    shadcn sheet

--- app/test1/page.tsx
+++ app/test1/page.tsx
@@ -1,62 +1,42 @@
 "use client"
-import { Fragment, useState } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
-import { EyeOpenIcon } from '@radix-ui/react-icons';
+import React from 'react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Cliptest from '@/components/Cliptest'
+import ClipboardCopy from '@/components/ClipboardCopy(old)'
+import CopyClipboard from '@/components/CopyClipboard'
 
-export default function Example() {
-  const [open, setOpen] = useState(false);
 
+export default function page() {
   return (
-    <div className="relative">
-      <button
-        className="fixed bottom-4 right-4 p-4 bg-blue-500 text-white rounded-full"
-        onClick={() => setOpen(true)}
-      >
-        <EyeOpenIcon className="h-6 w-6" />
-      </button>
+    <Sheet>
+  <SheetTrigger>Open</SheetTrigger>
+  <SheetContent side="bottom" className='h-[40%] rounded-2xl bg-zinc-800'>
+    <SheetHeader>
+      <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+      <SheetDescription>
+        This action cannot be undone. This will permanently delete your account
+      </SheetDescription>
+      
+      
 
-      <Transition.Root show={open} as={Fragment}>
-        <Dialog
-          as="div"
-          className="fixed inset-0 overflow-hidden"
-          onClose={() => setOpen(false)}
-        >
-          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Overlay className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
-            </Transition.Child>
+    </SheetHeader>
+    <div>
+      <p>Css - </p>
+      <p>Css - </p>
+      <p>Css - </p>
+      <Cliptest text='hello'/>
+      <ClipboardCopy text='hekssssssssklo'/>
+      {/* <CopyClipboard text="alla"/> */}
+      </div>
+  </SheetContent>
+</Sheet>
 
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <div className="fixed inset-0 transition-transform transform">
-                <div className="flex items-center justify-end h-screen">
-                  <button
-                    className="mr-4 mb-4 p-2 bg-red-500 text-white rounded-full"
-                    onClick={() => setOpen(false)}
-                  >
-                    <EyeOpenIcon className="h-6 w-6" />
-                  </button>
-                </div>
-              </div>
-            </Transition.Child>
-          </div>
-        </Dialog>
-      </Transition.Root>
-    </div>
-  );
+  )
 }
